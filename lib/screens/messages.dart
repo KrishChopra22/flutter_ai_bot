2fb82b3c1ff71bb31a99d44dd09eb14d07c8fcfe
Krish Chopra <krishchopra22@gmail.com>
2022-09-08 08:44:03 +0530

    added dialog_flow dependency

---
+++ lib/screens/messages.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Messages extends StatefulWidget {
+  final List messages;
+  const Messages({super.key, required this.messages});
+
+  @override
+  State<Messages> createState() => _MessagesState();
+}
+
+class _MessagesState extends State<Messages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
